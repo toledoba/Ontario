ff91f1a828fb60db61455b6397e1b4b640867996
Jhon Toledo Barreto <jhon.toledo.barreto@alumnos.upm.es>
2020-12-01 02:57:18 +0100

    updated

---
+++ run01_wop_12Endpoints.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo  "run,elapsed_time,kernel_mode,User_mode,memory_max,memory_average,file" >>timeQ1_wop.csv
+
+for i in 1 2 3 4 5 7 8 9 10
+do
+
+time  -f "Run,elapsed_time,kernel_mode,user_mode,memory_max,memory_average\n${i},%e,%S,%U,%M,%K," -o tmp_time.csv curl -G 'http://localhost:6001/sparql' \
+     --data-urlencode query='
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX dct: <http://purl.org/dc/terms/>
+
+SELECT * WHERE {
+?d a dcat:Dataset .
+?d dct:qualFreq ?qf .
+?d dct:metadataDate ?md .
+}'
+
+
+tail -1 tmp_time.csv>> timeQ1_wop.csv
+
+done
+rm tmp_time.csv
